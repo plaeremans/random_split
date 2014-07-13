1860d3614e4e9b3237d6410b924f0ceb6ce4b5ce
Pieter Laeremans <pieter@laeremans.org>
2014-07-13 21:58:27 +0200

    fixup

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # random-split
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to randomly split a file, 
+for example a csv file into two files according to a split factor
 
 ## Usage
 
-FIXME
+
+The following commands will split the input file in two files, 
+where the lines that end up in each file are determined at random.
+
+When the split factor is 0.5 each file wil contain an equal number of lines, if the input file has an even number of data rows.
+
+lein repl 
+(use 'random-split.core)
+(split-file "inputfile.csv" "f1" "f2" 0.5)
+
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 Pieter Laeremans
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/random_split/core.clj
+++ src/random_split/core.clj
@@ -2,17 +2,13 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(defn draw-random "draws r random numbers from the first n integers " [n r]
-  (repeatedly r #(Math/round (rand n) ) ))
-
-
 (defn write-to-file [f coll]
   (spit f (clojure.string/join "\n" coll)))
 
 (defn split-file [file-in file-out1 file-out2 split-factor]
   (with-open [in-file (io/reader file-in)]
     (let [in-rows (line-seq in-file)
-          header-row  (take 1 in-rows)
+          header-row  (first in-rows)
           content-rows (drop 1 in-rows)
           n  (count content-rows)
           r  (Math/round (* split-factor n))
@@ -22,10 +18,3 @@
       (do
         (write-to-file file-out1 (conj lines-for-one header-row))
         (write-to-file file-out2 (conj lines-for-two header-row))))))
-
-
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
